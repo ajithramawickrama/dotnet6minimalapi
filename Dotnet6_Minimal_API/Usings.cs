32ffcddfc87ae029957b111eafe89d95431fb57d
ajithramawickrama <ajith.ramawickrama@gmail.com>
2021-07-25 09:15:49 +0530

    Ading global usings

---
+++ Dotnet6_Minimal_API/Usings.cs
@@ -0,0 +1,14 @@
+global using System;
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.OpenApi.Models;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Routing;
+global using System.Linq;
+global using Microsoft.IdentityModel.Tokens;
+global using System.Text;
+global using Microsoft.AspNetCore.Authentication.JwtBearer;
+global using Microsoft.AspNetCore.Authorization;
+global using System.IdentityModel.Tokens.Jwt;
+
